c17ea0119b5dbcbc7f20b0a33ec765e39c4bd0a2
amiya12345 <50417230+amiya12345@users.noreply.github.com>
2022-12-20 09:40:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ data/BioData.js
@@ -0,0 +1,23 @@
+// date: 1 Aug, 2022
+const bioData = [
+    {
+        name: 'nxt lnk',
+        username: 'nxtlnk.vercel.app',
+        url: 'https://nxtlnk.vercel.app',
+        titleImg: true,
+        avatar: '/avatar.png',
+        nftAvatar: true,
+        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        subdesc: 'Use this template and deploy on vercel for free',
+        newProductUrl: 'https://3dicons.co',
+        newProduct: true,
+        author: 'realvjy',
+        authorURL: 'https://twitter.com/realvjy',
+        footerText: 'made by'
+    },
+];
+
+export default bioData;
+
+
+
